0e27d7f45da2745d05e52c33a66fada6407852c5
Jasmine Nie <junn@andrew.cmu.edu>
2022-09-21 20:07:50 -0400

    html changes again

--- src/main/java/com/develogical/web/IndexPage.java
+++ src/main/java/com/develogical/web/IndexPage.java
@@ -7,7 +7,7 @@ public class IndexPage extends HtmlPage {
     @Override
     protected void writeContentTo(PrintWriter writer) {
         writer.println(
-                "<h1>Welcome! This is Jasmine's Web App!!!</h1>" +
+                "<h1>Welcome! This is Jasmine Nie's Web App!!!</h1>" +
                 "<p>Enter your query in the box below: " +
                   "<form><input type=\"text\" name=\"q\" />" +
                     "<input type=\"submit\">" +
